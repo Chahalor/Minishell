fdeaeddf114f93ea4b7371b68f0367809c4f2a87
nduvoid <nduvoid@student.42mulhouse.fr>
2025-05-28 16:26:37 +0200

    add: new manager to manager -> fd manager

--- src/manager/mmanager.h
+++ src/manager/mmanager.h
@@ -6,7 +6,7 @@
 /*   By: nduvoid <nduvoid@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 12:12:01 by nduvoid           #+#    #+#             */
-/*   Updated: 2025/05/12 11:08:03 by nduvoid          ###   ########.fr       */
+/*   Updated: 2025/05/28 16:25:42 by nduvoid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@
 /*                                  Prototypes                                */
 /* ************************************************************************** */
 
+// mmanager.c
+
 void	*mm_alloc(
 			const size_t size
 			);
@@ -47,5 +49,27 @@ void	mm_free(
 			);
 
 void	mm_destroy(void);
+
+// fdmanager.c
+
+int		fdm_open(
+			const char *restrict path,
+			const int flags,
+			const int mode
+			);
+
+int		fdm_close(
+			const int fd
+			);
+
+int		fdm_register(
+			const int fd
+			);
+
+int		fdm_is_open(
+			const int fd
+			);
+
+int		fdm_close_all(void);
 
 #endif /* MMANAGER_H */
