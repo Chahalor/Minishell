dd3b7336275ddc187b8ef51cfac3810fa9ae7ab5
nduvoid <nduvoid@student.42mulhouse.fr>
2025-06-05 10:51:39 +0200

    add: builtin: cd with handling of HOME and OLDPWD

--- src/builtin/_internal/_builtin.h
+++ src/builtin/_internal/_builtin.h
@@ -6,7 +6,7 @@
 /*   By: nduvoid <nduvoid@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 14:15:37 by nduvoid           #+#    #+#             */
-/*   Updated: 2025/06/05 09:48:15 by nduvoid          ###   ########.fr       */
+/*   Updated: 2025/06/05 10:38:11 by nduvoid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define _BUILTIN_H
 
 # pragma once
+
+# include "formating.h"
 
 /* ************************************************************************** */
 /*                                 Typedefs                                   */
@@ -25,7 +27,12 @@ typedef struct s_blt_link	t_blt_link;	/* Builtin link structure */
 /*                                 Enums                                      */
 /* ************************************************************************** */
 
-//...
+enum e_echo_error
+{
+	echo_error_none = 0,			/* No error                          */
+	echo_error_too_many_args = 1,	/* Too many arguments error         */
+	echo_error_no_such_file = 2,	/* No such file or directory error */
+};
 
 /* ************************************************************************** */
 /*                                 Structs                                    */
@@ -43,5 +50,12 @@ struct s_args_echo
 	unsigned char	help	: 1;	/* Help flag             */
 };
 
+struct s_args_cd
+{
+	enum e_echo_error	error;			/* Error flag            */
+	unsigned char		help	: 1;	/* Help flag            */
+	unsigned char		oldpwd	: 1;	/* Oldpwd flag         */
+	unsigned char		home	: 1;	/* Pwd flag           */
+};
 
 #endif /* _BUILTIN_H */
