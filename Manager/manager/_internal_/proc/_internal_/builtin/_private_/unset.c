65d36457b108a6a4fcd76fb65b2a1efad4ba1a30
nduvoid <nduvoid@student.42mulhouse.fr>
2025-07-25 15:58:35 +0200

    auto push

--- Manager/manager/_internal_/proc/_internal_/builtin/_private_/unset.c
+++ Manager/manager/_internal_/proc/_internal_/builtin/_private_/unset.c
@@ -6,7 +6,7 @@
 /*   By: nduvoid <nduvoid@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 14:14:22 by nduvoid           #+#    #+#             */
-/*   Updated: 2025/07/25 13:03:58 by nduvoid          ###   ########.fr       */
+/*   Updated: 2025/07/25 15:58:33 by nduvoid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,9 +41,14 @@ static inline int	_parse(
 	const int		argc = arraylen((const void *const *)args);
 	register int	i;
 
-	if (unexpect(!args || !*args))
+	if (unexpect(!args || !*args || argc < 2))
+		return (-1);
 	i = -1;
 	while (++i < argc)
+		if (ft_strncmp(args[i], "-h", 3) == 0
+			|| ft_strcmp(args[i], "--help", 7) == 0)
+			return (1);
+	return (0);
 }
 
 /** */
